ff6cba1e9db394734ef9cf523245eb4d17c1e30f
Frank Vitor <frank.vitor2013@gmail.com>
2019-02-14 23:04:27 -0200

    ex8

---
+++ ConsoleApp8/Calculadora.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp8
+{
+    public class Calculadora
+    {
+        public double Somar(double valor1, double valor2)
+        {
+            double soma = valor1 + valor2;
+            Console.WriteLine(soma);
+            return soma;
+        }
+        public double Subtrair(double valor1, double valor2)
+        {
+            double subitrair = valor1 - valor2;
+            Console.WriteLine(subitrair);
+            return subitrair;
+        }
+        public double Dividir(double valor1, double valor2)
+        {
+            double dividir = valor1 / valor2;
+            Console.WriteLine(dividir);
+            return dividir;
+        }
+        public double Multiplicar(double valor1, double valor2)
+        {
+            double multiplicar = valor1 * valor2;
+            Console.WriteLine(multiplicar);
+            return multiplicar;
+        }
+    }
+}
---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora calc = new Calculadora();
+            double valor1, valor2;
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("*** Para iniciar a operação basta primeiro digitar um numero\n na sequencia escolher a operação desejada e digitar o próximo valor***\n\n");
+            Console.ResetColor();
+
+            Console.WriteLine("");
+            var vlr1 = Console.ReadLine();
+            double.TryParse(vlr1, out valor1);
+
+            Console.WriteLine(" +\n -\n /\n *");
+            var entrada = Console.ReadLine();
+
+            Console.WriteLine("");
+            var vlr2 = Console.ReadLine();
+            double.TryParse(vlr2, out valor2);
+
+            if (entrada.Equals("+"))
+            {
+                calc.Somar(valor1, valor2);
+            }
+            else if (entrada.Equals("-"))
+            {
+                calc.Subtrair(valor1, valor2);
+            }
+            else if (entrada.Equals("/"))
+            {
+                calc.Dividir(valor1, valor2);
+            }
+            else
+            {
+                calc.Multiplicar(valor1, valor2);
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
